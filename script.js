80765f7f30ba6f847a244736cdcb29aaf17d2265
sbagazinska <82549156+sbagazinska@users.noreply.github.com>
2021-06-18 00:20:13 +0200

    week7

--- script.js
+++ script.js
@@ -5,36 +5,38 @@ let day = days[now.getDay()];
 let time = document.querySelector("#time");
 let hours = ("0" + now.getHours()).slice(-2);
 let minutes = ('0' + now.getMinutes()).slice(-2);
+
+let iconElement = document.querySelector("#icon");
+
 today.innerHTML = `${day}`;
 time.innerHTML = `${hours}:${minutes}`;
-
-function convertToFahrenheit(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = temperatureElement.innerHTML;
-    temperatureElement.innerHTML = 66;
-}
-
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-function convertToCelsius(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 19;
-}
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", convertToCelsius);
-
 
 function displayWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#precipitation").innerHTML = Math.round(response.data.precipitation);
     document.querySelector("#description").innerHTML = response.data.weather[0].main;
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
+    celciusTemperature = response.data.main.temp;
 }
+
+function convertToFahrenheit(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 function searchCity(city) {
     let apiKey = "c59a32485cb0c9976a8d0efdab172848";
@@ -67,3 +69,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#btn-current");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 searchCity("Warsaw");
+
